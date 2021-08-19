9a2159bcb836b64e5246e5eb5a13ded81ba0c667
honor_stone <honor_stone@163.com>
2021-08-19 16:43:39 +0800

    lib-jar applications

---
+++ lib-jar/src/main/java/me/stone/training/applications/lib/jars/Launcher.java
@@ -0,0 +1,16 @@
+package me.stone.training.applications.lib.jars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author honorStone
+ * @version 1.0
+ * @since 8/18/21 3:17 PM
+ */
+@SpringBootApplication
+public class Launcher {
+    public static void main(String[] args) {
+        SpringApplication.run(Launcher.class,args);
+    }
+}
